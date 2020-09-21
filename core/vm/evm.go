c5b92900f487dd07f80fc99f17ee0633b070f165
Martin Holst Swende <martin@swende.se>
2020-09-21 12:46:36 +0200

    core/vm: lint nitpicks

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -432,7 +432,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	evm.StateDB.SetNonce(caller.Address(), nonce+1)
 	// We add this to the access list _before_ taking a snapshot. Even if the creation fails,
 	// the access-list change should not be rolled back
-	if evm.chainRules.IsYoloV2{
+	if evm.chainRules.IsYoloV2 {
 		evm.StateDB.AddAddrToAccessList(address)
 	}
 	// Ensure there's no existing contract already at the designated address
