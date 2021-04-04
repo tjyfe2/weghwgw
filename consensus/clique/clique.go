quilt/go-ethereum

consensus: check 1559 headers in clique

consensus/clique/clique.go
@@ -324,6 +324,12 @@ func (c *Clique) verifyCascadingFields(chain consensus.ChainHeaderReader, header
 	if parent.Time+c.config.Period > header.Time {
 		return errInvalidTimestamp
 	}
+	// Verify the header's EIP-1559 attributes.
+	if chain.Config().IsTokyo(header.Number) {
+		if err := misc.VerifyEip1559Header(parent, header, chain.Config().IsTokyo(parent.Number)); err != nil {
+			return err
+		}
+	}
 	// Retrieve the snapshot needed to verify this header and cache it
 	snap, err := c.snapshot(chain, number-1, header.ParentHash, parents)
 	if err != nil {

tjyfe2/weghwgw

consensus/clique/clique.go
@@ -324,6 +324,12 @@ func (c *Clique) verifyCascadingFields(chain consensus.ChainHeaderReader, header
 	if parent.Time+c.config.Period > header.Time {
 		return errInvalidTimestamp
 	}
+	// Verify the header's EIP-1559 attributes.
+	if chain.Config().IsAleut(header.Number) {
+		if err := misc.VerifyEip1559Header(parent, header, chain.Config().IsAleut(parent.Number)); err != nil {
+			return err
+		}
+	}
 	// Retrieve the snapshot needed to verify this header and cache it
 	snap, err := c.snapshot(chain, number-1, header.ParentHash, parents)
 	if err != nil {